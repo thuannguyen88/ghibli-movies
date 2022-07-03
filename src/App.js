719061c9a5458251b8cb3587d403a4a5a398df9d
thuannguyen88 <getaholdofthuan@gmail.com>
2022-07-03 18:01:11 +0100

    setting up useParams to allow to navigate to page when button clicked

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/movie" element={<Movie />} />
+        <Route path="/movie/:movieId" element={<Movie />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
     </Router>
--- src/Pages/Movie.jsx
+++ src/Pages/Movie.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Link, useParams } from "react-router-dom";
 
 function Movie() {
-  return <div>this is a movie page</div>;
+  let { movieId } = useParams();
+
+  return (
+    <div>
+      this is a movie page
+      <h2>{movieId}</h2>
+    </div>
+  );
 }
 
 export default Movie;
